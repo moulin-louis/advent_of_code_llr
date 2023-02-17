bda3cc38cae59552ddfc7cd03fd435bd4629c0a9
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-02-17 13:02:19 +0100

    finish day3 AOC

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -9,11 +9,11 @@ fn open_file( file_path:&str ) -> String {
 		Err(_e) => panic!("Failed to open file\n"),
 		Ok(x) => file = x,
 	};
-	let mut input = String::new();
 	let mut buffer = BufReader::new(file);
+	let	mut result = String::new();
 
-	buffer.read_to_string(&mut input).unwrap();
-	input
+	buffer.read_to_string(&mut result).unwrap();
+	result
 }
 
 //calculate the size of the curent line
@@ -24,26 +24,6 @@ fn size_len( input: &[u8], pos:usize ) -> usize {
 		}
 	}
 	return input.len() - pos;
-}
-
-//calculate the size of the next 3 lines
-fn len_3_line( input: &[u8], pos_1:usize ) -> usize {
-	let mut nbr_line:i32 = 0;
-
-	let mut pos = pos_1;
-	if input[pos] as char == '\n' {
-		pos += 1;
-	}
-	for i in pos..input.len() {
-		if input[i] as char == '\n' {
-			nbr_line += 1;
-		}
-		if nbr_line == 3 {
-			return  i;
-		}
-	}
-	println!("cant find 3 line");
-	input.len() - pos
 }
 
 //calculate what is the priority of the item
@@ -101,50 +81,34 @@ fn calculate_priority( input: &[u8], pos: usize ) -> i32 {
 }
 
 //Part two
-fn fing_badge( input: &[u8], pos_1:usize) -> i32 {
-	let mut pos:usize = pos_1;
-	if input[pos] as char == '\n' {
-		pos += 1;
-	}
-	let mut sz_len = size_len(input, pos);
+fn find_badge( line_1:&str, line_2:&str, line_3:&str) -> i32 {
 
-	//println!("pos = {}", pos);
 	let mut arr_1: [u8; u8::MAX as usize] = [0; u8::MAX as usize];
-	for i in pos..(pos + sz_len) {
-		if input[i] as char == '\n' {
+	for i in line_1.chars() {
+		if i == '\n' {
 			break ;
 		}
-		let it = input[i] as usize;
-		arr_1[it] = 1;
+		let it = i as usize;
+		arr_1[it] = 1
 	}
 
 	let mut arr_2: [u8; u8::MAX as usize] = [0; u8::MAX as usize];
-	pos += sz_len + 1;
-	//println!("pos = {}", pos);
-	sz_len = size_len(input, pos);
-	for i in pos..(pos + sz_len) {
-		if input[i] as char == '\n' {
+	for i in line_2.chars() {
+		if i == '\n' {
 			break ;
 		}
-		let it = input[i] as usize;
-		arr_2[it] = 1;
+		let it = i as usize;
+		arr_2[it] = 1
 	}
 
 	let mut arr_3: [u8; u8::MAX as usize] = [0; u8::MAX as usize];
-	pos += sz_len + 1;
-	//println!("pos = {}", pos);
-	sz_len = size_len(input, pos);
-	for i in pos..(pos + sz_len) {
-		if input[i] as char == '\n' {
+	for i in line_3.chars() {
+		if i == '\n' {
 			break ;
 		}
-		let it = input[i] as usize;
-		arr_3[it] = 1;
+		let it = i as usize;
+		arr_3	[it] = 1
 	}
-
-	// println!("{:?}\n", arr_1);
-	// println!("{:?}\n", arr_2);
-	// println!("{:?}\n", arr_3);
 
 	for i in 0..arr_1.len() {
 		if arr_2[i] == 1 {
@@ -154,23 +118,23 @@ fn fing_badge( input: &[u8], pos_1:usize) -> i32 {
 			arr_1[i] += 1;
 		}
 	}
-	//println!("{:?}", arr_1);
+
 	for i in 0..arr_1.len() {
 		if arr_1[i] == 3 {
-			//println!("found a badge");
 			return what_priority(i);
 		}
 	}
 	0
 }
 
+
 fn main() {
 	let mut result:i32 = 0;
 	let input = open_file("input");
 	let input: &[u8] = input.as_bytes();
-	let mut  i = 0;
 
 	//Part one
+	let mut i:usize = 0;
 	while i < input.len() {
 		result += calculate_priority(input, i);
 
@@ -180,16 +144,16 @@ fn main() {
 		}
 	}
 	println!("result of part one = {}", result);
+
 	//Part two
+	let input = open_file("input");
+	let mut _n:i32 = 0;
 	result = 0;
-	i = 0;
-	let mut nbr_call = 0;
-	while i < input.len() {
-		nbr_call += 1;
-		result += fing_badge(input, i);
-		i += len_3_line(input, i);
-		println!("i = {}", i);
+	for _i in input.lines().step_by(3) {
+		result += find_badge(input.lines().nth(_n as usize).unwrap(),
+							 input.lines().nth((_n + 1) as usize).unwrap(),
+							 input.lines().nth((_n + 2 )as usize).unwrap());
+		_n += 3;
 	}
-	println!("nbr call = {}", nbr_call);
-	println!("result of part two = {}", result);
+	println!("result part two = {}", result);
 }
